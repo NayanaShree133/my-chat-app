c4d2fa788c3e142cc95e54772ccd043723bb7e3e
nayana <nayanas133@gmail.com>
2025-11-28 15:15:51 +0530

    Initial commit with cicd pipeline

--- lib/my-chat-app-stack.ts
+++ lib/my-chat-app-stack.ts
@@ -1,16 +1,99 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class MyChatAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB Table for storing messages
+    const messagesTable = new dynamodb.Table(this, 'MessagesTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'timestamp', type: dynamodb.AttributeType.NUMBER },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // For dev only
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyChatAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function for handling messages
+    const chatLambda = new lambda.Function(this, 'ChatHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      environment: {
+        TABLE_NAME: messagesTable.tableName,
+      },
+      timeout: cdk.Duration.seconds(30),
+    });
+
+    // Grant Lambda permissions to read/write to DynamoDB
+    messagesTable.grantReadWriteData(chatLambda);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'ChatApi', {
+      restApiName: 'Chat Service',
+      description: 'API for chat application',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: ['Content-Type', 'Authorization'],
+      },
+    });
+
+    // Lambda integration
+    const lambdaIntegration = new apigateway.LambdaIntegration(chatLambda);
+
+    // API routes
+    const messages = api.root.addResource('messages');
+    messages.addMethod('GET', lambdaIntegration); // Get all messages
+    messages.addMethod('POST', lambdaIntegration); // Post a new message
+
+    // S3 bucket for frontend hosting
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // Deploy frontend to S3
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../frontend'))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+      description: 'API Gateway URL',
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteUrl', {
+      value: distribution.distributionDomainName,
+      description: 'CloudFront Website URL',
+    });
+
+    new cdk.CfnOutput(this, 'TableName', {
+      value: messagesTable.tableName,
+      description: 'DynamoDB Table Name',
+    });
   }
 }
